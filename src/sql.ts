910f840d8ba585c2bc128e2ea2cf33edfbd00358
Simen Bjorkhaug <simen@bjorkhaug.io>
2023-12-04 17:47:02 +0100

    feat: add literal value escape for sql

--- src/sql.ts
+++ src/sql.ts
@@ -1,12 +1,7 @@
-export function sql(args: TemplateStringsArray, ...params: unknown[]) {
-  let query = ''
-  for (let i = 0; i < args.length; i++) {
-    query += args[i]
-    if (i < params.length) {
-      query += '?'
-    }
-  }
-  return [query.trim().replace(/\s*\n\s*/g, '\n'), params]
+const SQL_LITERAL = Symbol('SQL_LITERAL')
+
+export function literal(value: string) {
+  return { [SQL_LITERAL]: value }
 }
 
 export function createSqlFunction(
@@ -17,16 +12,29 @@ export function createSqlFunction(
   const sql_placeholder = parameter_replace_value
   return function sql(args: TemplateStringsArray, ...params: unknown[]) {
     let query = ''
+
+    const params_shallow_copy = [...params]
+
     for (let i = 0; i < args.length; i++) {
+      const param = params[i]
+
       query += args[i]
       if (i < params.length) {
-        query += sql_placeholder
+        if (param && typeof param === 'object' && SQL_LITERAL in param) {
+          query += param[SQL_LITERAL]
+          params_shallow_copy.splice(i, 1)
+        } else {
+          query += sql_placeholder
 
-        if (use_index) {
-          query += i + start_at_index
+          if (use_index) {
+            query += i + start_at_index
+          }
         }
       }
     }
-    return [query.trim().replace(/\s*\n\s*/g, '\n'), params]
+
+    return [query.trim().replace(/\s*\n\s*/g, '\n'), params_shallow_copy]
   }
 }
+
+export const sql = createSqlFunction('?')
